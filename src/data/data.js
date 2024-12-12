bb59d4a0fbeda9a414a6751643daa89bb69e8bf3
Andreas <201908525@post.au.dk>
2024-12-12 18:33:25 +0100

    data update

--- src/data/data.js
+++ src/data/data.js
@@ -1,28 +1,29 @@
 import Upcoming from "../assets/projects/UPCOMING.png";
 import Dashboard from "../assets/projects/dashboard.png";
 import Eatnsplit from "../assets/projects/eatnsplit.png";
+import Padel from "../assets/projects/padel.png";
 
 export const data = [
   {
     id: 1,
     name: "Mexicano Padel",
-    image: Upcoming,
+    image: Padel,
     github: "https://github.com/AndreasHoegh/mexicano-padel",
     live: "https://mexicano-padel-y36f.vercel.app/",
   },
   {
     id: 2,
     name: "eat-n-split React",
-    image: Eatnsplit,
+    image: Upcoming,
     github: "https://github.com/AndreasHoegh/eat-n-split",
-    live: "https://eat-n-split-blond-chi.vercel.app/",
+    live: "",
   },
   {
     id: 3,
     name: "Next.js Dashboard",
-    image: Dashboard,
+    image: Upcoming,
     github: "https://github.com/AndreasHoegh/nextjs-dashboard",
-    live: "https://nextjs-dashboard-eight-eta-10.vercel.app/",
+    live: "",
   },
   {
     id: 4,
